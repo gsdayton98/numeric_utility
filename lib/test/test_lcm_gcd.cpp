08e0ae9fdf74dfdf16d0acd4cb8f47d9c7b2bda6
Glen Dayton <dayton@ieee.org>
2025-08-29 14:26:50 -0700

    Add more lcm and fcd tests.

--- lib/test/test_lcm_gcd.cpp
+++ lib/test/test_lcm_gcd.cpp
@@ -12,3 +12,20 @@ BOOST_AUTO_TEST_CASE(test_gcd)
     BOOST_CHECK_EQUAL(numutil::greatestCommonDivisor(0, 12), 12);
     BOOST_CHECK_EQUAL(numutil::greatestCommonDivisor(18, 1), 1);
 }
+
+
+BOOST_AUTO_TEST_CASE(test_lcm)
+{
+    BOOST_CHECK_EQUAL(numutil::leastCommonMultiple(12, 18), 36);
+    BOOST_CHECK_EQUAL(numutil::leastCommonMultiple(18, 12), 36);
+    BOOST_CHECK_EQUAL(numutil::leastCommonMultiple(18, 0), 0);
+    BOOST_CHECK_EQUAL(numutil::leastCommonMultiple(0, 18), 0);
+    BOOST_CHECK_EQUAL(numutil::leastCommonMultiple(1, 18), 18);
+    BOOST_CHECK_EQUAL(numutil::leastCommonMultiple(18, 1), 18);
+}
+
+BOOST_AUTO_TEST_CASE(test_vector_lcm)
+{
+    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    BOOST_CHECK_EQUAL(numutil::leastCommonMultiple(v), 2520);
+}
